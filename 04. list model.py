d281b62a75923629d91ceb6c85e585a06d4339b0
Galih Hermawan <galih.hermawan@gmail.com>
2024-11-20 21:15:14 +0700

    Update 04. list model.py

--- 04. list model.py
+++ 04. list model.py
@@ -3,7 +3,7 @@
 import google.generativeai as genai
 
 # Token API ditulis langsung di sini untuk kemudahan (tidak disarankan untuk produksi)  
-API_KEY = "AIzaSyBomyKrBlHUaRkzLf7Iu4aJEcibhxgneM4"  
+API_KEY = "API_KEY_ANDA"  
 
 # Konfigurasikan API Key Anda
 API_KEY = "API_KEY_ANDA"  
